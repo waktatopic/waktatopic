c91da759170e68753aa05a5110428df53b36e218
young314 <swine9866@gmail.com>
2023-09-11 15:09:45 +0900

    .

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
 	res.sendFile(path.join(initialPath, "html", "home.html"));
 });
 
+app.get("/book", (req, res) => {
+	res.sendFile(path.join(initialPath, "html", "book.html"));
+});
+
 app.listen("3000", () => {
-	console.log("listening...");
+	console.log("listening on port 3000");
 });
---
+++ public/js/book.js
@@ -0,0 +1,18 @@
+$(".solid-container").FlipBook({
+	pdf: "../pdf/test.pdf",
+	template: {
+		html: "../library/3d-flip-book/3d-flip-book/templates/default-book-view.html",
+		styles: ["../library/3d-flip-book/3d-flip-book/css/black-book-view.css"],
+		links: [
+			{
+				rel: "stylesheet",
+				href: "../library/3d-flip-book/3d-flip-book/css/font-awesome.min.css",
+			},
+		],
+		script: "../library/3d-flip-book/3d-flip-book/js/default-book-view.js",
+		sounds: {
+			startFlip: "../library/3d-flip-book/3d-flip-book/sounds/start-flip.mp3",
+			endFlip: "../library/3d-flip-book/3d-flip-book/sounds/end-flip.mp3",
+		},
+	},
+});
